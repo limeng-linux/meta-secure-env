7b32c72a105de7c74aa311dc3f3514b6b65b2734
Lans Zhang <jia.zhang@windriver.com>
2017-04-26 15:42:50 +0800

    rpm: fix key importing path
    Signed-off-by: Lans Zhang <jia.zhang@windriver.com>

--- recipes-devtools/rpm/files/rpm-key-import.sh
+++ recipes-devtools/rpm/files/rpm-key-import.sh
@@ -4,9 +4,7 @@ keydir="/etc/pki/rpm-gpg"
 
 [ ! -d "$keydir" ] && exit 0
 
-for f in `ls $keydir/RPM-GPG-KEY-*`; do
-    keyfile="$keydir/$f"
-
+for keyfile in `ls $keydir/RPM-GPG-KEY-*`; do
     [ ! -f "$keyfile" ] && continue
 
     ! rpm --import "$keyfile" &&
